536aab44b119f383d2a76905a758f72c6fe475b5
jiyeon11 <leejiyeon415@gmail.com>
2023-11-13 09:11:06 +0900

    ✨ 주먹 효과음 추가

--- Assets/script/PunchScene/PunchGameScript.cs
+++ Assets/script/PunchScene/PunchGameScript.cs
@@ -16,9 +16,13 @@ public class PunchGameScript : MonoBehaviour
     KeyCode randomKey;  // 랜덤으로 선택된 키를 저장할 변수
     public int count = 0;  // 주먹 몇 번 생성됐는지
 
+    AudioSource audioSource;
+    public AudioClip punchSound;
+
     // 게임 시작 함수
     public void GameStart()
     {
+        audioSource = GetComponent<AudioSource>();
         howToPlayPanel.SetActive(false);  // 게임방법 패널 비활성화
         gaugeBar.SetActive(true);  // 체력바 활성화
         bubbleText.text = "어디 한 번 막아보시지!";  // 말풍선 대사 설정
@@ -77,11 +81,13 @@ public class PunchGameScript : MonoBehaviour
             // 주먹 판정선이 주먹과 안맞으면 실패
             if (Input.GetKeyDown(randomKey) && newScale <= 54 && newScale >= 47f)
             {
+                audioSource.PlayOneShot(punchSound);
                 Pass();  // 성공
                 yield break;
             }
             else if(Input.anyKeyDown || newScale == 47f)
             {
+                audioSource.PlayOneShot(punchSound);
                 Fail();  // 실패
                 yield break;
             }
