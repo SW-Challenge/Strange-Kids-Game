022dfa37438e233f531ead22a88b23800456257b
jiyeon11 <leejiyeon415@gmail.com>
2023-11-14 23:11:59 +0900

    ✨ 주먹이 게이지바와 말풍선에 안 겹쳐지게 & 잼민이 성공, 실패 시 대사 추가

--- Assets/script/PunchScene/PunchGameScript.cs
+++ Assets/script/PunchScene/PunchGameScript.cs
@@ -14,8 +14,11 @@ public class PunchGameScript : MonoBehaviour
     public TMP_Text bubbleText;  // 잼민이 말풍선 대사
     public GameObject jemminAni;  //잼민이 애니메이션
     public GameObject jemmin;  //잼민이
-    KeyCode randomKey;  // 랜덤으로 선택된 키를 저장할 변수
     public int count = 0;  // 주먹 몇 번 생성됐는지
+    KeyCode randomKey;  // 랜덤으로 선택된 키를 저장할 변수
+
+    string[] failText = {"슈슈슉슈슉ㅋㅋ 못 막았쥬?","아~ 쇼쇼쇽쇼쇽 아뵤~","내 주먹은 네 눈보다 빠르지","어린애한테 지면 안 창피해?"};
+    string[] passText = {"오;; 꽤 하는데?","지금까진 진심모드가 아니였어!","이젠 진짜 진심으로 때린다!!","크윽...."};
 
     AudioSource audioSource;
     public AudioClip punchSound;
@@ -23,6 +26,7 @@ public class PunchGameScript : MonoBehaviour
     // 게임 시작 함수
     public void GameStart()
     {
+        GameObject.Find("click").GetComponent<AudioSource>().Play();
         jemmin.SetActive(false);  //잼민이 비활성화
         jemminAni.SetActive(true);  //잼민이 애니메이션 활성화
         audioSource = GetComponent<AudioSource>();
@@ -35,8 +39,7 @@ public class PunchGameScript : MonoBehaviour
     // 성공
     void Pass()
     {
-        bubbleText.text = "오; 꽤 하는데?";
-        Debug.Log(randomKey + " : 성공");
+        bubbleText.text = passText[Random.Range(0, passText.Length - 1)];  //성공 대사
         Check();
     }
 
@@ -49,8 +52,7 @@ public class PunchGameScript : MonoBehaviour
     // 실패
     void Fail()
     {
-        Debug.Log(randomKey + " : 실패");
-        bubbleText.text = "슈슈슉슈슉ㅋㅋ 못 막았쥬?";
+        bubbleText.text = failText[Random.Range(0, failText.Length - 1)];  //실패 대사
         gaugeBarImage.fillAmount -= 0.1f;  // 체력 감소
         Check();
     }
@@ -58,7 +60,7 @@ public class PunchGameScript : MonoBehaviour
     // 랜덤 딜레이 후 주먹 생성 코루틴
     IEnumerator RandomDelayedSpawn()
     {
-        yield return new WaitForSeconds(Random.Range(1f, 5f));  // 1초에서 5초 사이의 랜덤한 시간 딜레이
+        yield return new WaitForSeconds(Random.Range(1f, 4f));  // 1초에서 4초 사이의 랜덤한 시간 딜레이
         SpawnFist();  // 주먹 생성
         fist.SetActive(true);  // 주먹 활성화
         fistOutline.SetActive(true);  // 주먹 판정선 활성화
@@ -69,7 +71,7 @@ public class PunchGameScript : MonoBehaviour
     {
         float targetScale = 47f;
         float startScale = fistOutline.transform.localScale.x;
-        float speed = 30f;  // 일정 속도
+        float speed = 45f;  // 일정 속도
 
         while (fistOutline.transform.localScale.x > targetScale)
         {
@@ -82,7 +84,7 @@ public class PunchGameScript : MonoBehaviour
             fistOutline.transform.localScale = new Vector3(newScale, newScale, 1f);
 
             // 주먹 판정선이 주먹과 안맞으면 실패
-            if (Input.GetKeyDown(randomKey) && newScale <= 54 && newScale >= 47f)
+            if (Input.GetKeyDown(randomKey) && newScale <= 54f && newScale >= 47f)
             {
                 audioSource.PlayOneShot(punchSound);
                 Pass();  // 성공
@@ -102,31 +104,30 @@ public class PunchGameScript : MonoBehaviour
     void SpawnFist()
     {
         count++;
-
         // 랜덤 키
         char randomChar = (char)Random.Range('A', 'Z' + 1);
         randomKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), randomChar.ToString());
 
-        // 랜덤 위치 설정
-        Vector3 randomPosition = GetRandomPosition();
-        // 주먹 위치 설정
-        fist.transform.position = randomPosition;
+        // 주먹 랜덤 위치 설정
+        fist.transform.position = GetRandomPosition();
         // 키 텍스트 설정
         keyText.text = randomKey.ToString();
-
+        // 키 위치 설정
+        keyText.transform.position = fist.GetComponent<Renderer>().bounds.center;
         // 주먹 판정선 크기 초기화
         fistOutline.transform.localScale = new Vector3(80f, 80f, 1f);
         // 주먹 판정선 위치 설정
         fistOutline.transform.position = fist.GetComponent<Renderer>().bounds.center;
-        // fistOutline.transform.position = randomPosition;
+        //판정선 애니메이션 시작
         StartCoroutine(ScaleAnimation());
     }
 
     // 랜덤 위치 생성 함수
     Vector3 GetRandomPosition()
     {
+        //게이지바와 말풍선에 안겹쳐지게
         float x = Random.Range(Screen.width * 0.1f, Screen.width * 0.9f);
-        float y = Random.Range(Screen.height * 0.2f, Screen.height * 0.6f);
+        float y = Random.Range(700,450);
         return new Vector3(x, y, 1f);
     }
 }
