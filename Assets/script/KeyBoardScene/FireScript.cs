654b293a6ab9598cfb8a4bc1ff2e984f1026743f
MsEmily1020 <peace10200khs@gmail.com>
2023-11-15 19:42:57 +0900

    ✨ fire script 생성 8초마다 실행

---
+++ Assets/script/KeyBoardScene/FireScript.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+using TMPro;
+using Random = UnityEngine.Random;
+
+public class FireScript : MonoBehaviour
+{
+    public GameObject fire;
+    public float bulletSpeed = 10f; // ÃÑ¾Ë ¼Óµµ
+    float x = 782.0f, y = -53.0f;
+
+    void Start()
+    {
+        InvokeRepeating("Fire", 8f, 8f);
+    }
+
+    void Update()
+    {
+        fire.transform.Translate(Vector2.left * (bulletSpeed * Time.deltaTime + 5));
+    }
+
+    void Fire()
+    {
+        fire.transform.localPosition = new Vector3(x, y, 1f);
+        Debug.Log(fire.transform.position);
+    }
+}
