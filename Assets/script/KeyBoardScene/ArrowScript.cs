99c60fa6572ecc14b15933c0eb417a0fbbce5183
MsEmily1020 <peace10200khs@gmail.com>
2023-11-15 10:08:24 +0900

    ✨ 공룡 브레스 기능 구현

---
+++ Assets/script/KeyBoardScene/ArrowScript.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+using TMPro;
+using Random = UnityEngine.Random;
+using System.Runtime.ExceptionServices;
+
+
+public class ArrowScript : MonoBehaviour
+{
+    public GameObject arrow;
+
+    void Start()
+    {
+        arrow.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
