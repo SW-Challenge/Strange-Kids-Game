1f88876d9fabfb36bdd10277121b0152b0cf8935
MsEmily1020 <peace10200khs@gmail.com>
2023-11-16 12:06:02 +0900

    ✨ 화살 충돌 및 플레이어 충돌 시 기능 구현

--- Assets/script/KeyBoardScene/ArrowScript.cs
+++ Assets/script/KeyBoardScene/ArrowScript.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+using TMPro;
+using Random = UnityEngine.Random;
 
 public class ArrowScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject arrow;
+    float bulletSpeed = 10f; // ÃÑ¾Ë ¼Óµµ
+    float x = -764.8f, y = -68.5f;
+
     void Start()
     {
-        
+        arrow.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        arrow.transform.Translate(Vector2.right * (bulletSpeed * Time.deltaTime + 5));
+    }
+
+    public void Arrow()
+    {
+        arrow.SetActive(true);
+        arrow.transform.localPosition = new Vector3(x, y, 1f);
     }
 }
