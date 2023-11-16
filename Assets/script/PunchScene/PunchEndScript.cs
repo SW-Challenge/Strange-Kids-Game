c3bd6a9473a6786d7684d419b4f088e0c9f11fe8
jiyeon11 <leejiyeon415@gmail.com>
2023-11-16 14:45:28 +0900

    💡 주먹 막기 스크립트 주석 수정

--- Assets/script/PunchScene/PunchEndScript.cs
+++ Assets/script/PunchScene/PunchEndScript.cs
@@ -6,8 +6,8 @@ using TMPro;
 
 public class PunchEndScript : MonoBehaviour
 {
-    public static GameObject endPanel;  //게임종료 패널
-    public static TMP_Text content;
+    public static GameObject endPanel;  // 게임종료 패널
+    public static TMP_Text content;  // 게임 종료 대사
 
     public void Awake()
     {
@@ -18,7 +18,7 @@ public class PunchEndScript : MonoBehaviour
 
     public static void GameOver()
     {
-        //주먹막기를 10번 했거나 게이지가 0이 되면 게임 종료
+        // 주먹막기를 10번 했거나 게이지가 0이 되면 게임 종료
         if (GameObject.FindObjectOfType<PunchGameScript>().gaugeBarImage.fillAmount == 0 || GameObject.FindObjectOfType<PunchGameScript>().count == 10)
         {
             GameObject.FindObjectOfType<PunchGameScript>().StopCoroutine("RandomDelayedSpawn");
@@ -26,7 +26,7 @@ public class PunchEndScript : MonoBehaviour
             GameObject.FindObjectOfType<PunchGameScript>().jemminAni.SetActive(false);  //잼민이 애니메이션 비활성화
             GameObject.FindObjectOfType<PunchGameScript>().jemmin.SetActive(true);  //잼민이 활성화
 
-            if(GameObject.FindObjectOfType<PunchGameScript>().gaugeBarImage.fillAmount != 0){
+            if(GameObject.FindObjectOfType<PunchGameScript>().gaugeBarImage.fillAmount >= 0.3){
                 content.text = "휴.. 초등학생의 주먹을 잘 막았다.\n앞으로 또 이러진 않겠지?";
             }else{
                 content.text = "내가 어린애한테 지다니...";
